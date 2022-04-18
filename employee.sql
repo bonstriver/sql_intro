7c12caa65e637877948c4e04d56e01fd811cd74b
bonstriver <brandon.martinez0502@gmail.com>
2022-04-18 15:19:07 -0700

    finished employee

---
+++ employee.sql
@@ -0,0 +1,17 @@
+SELECT first_name, last_name FROM employee
+WHERE city LIKE 'Calgary%';
+
+SELECT * FROM employee
+ORDER BY birth_date DESC
+LIMIT 1;
+
+SELECT * FROM employee
+ORDER BY birth_date ASC
+LIMIT 1;
+
+SELECT * FROM employee
+WHERE reports_to = 2;
+
+SELECT COUNT(city)
+FROM employee
+WHERE city LIKE '%Lethbridge%';
